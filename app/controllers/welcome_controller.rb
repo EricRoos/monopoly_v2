870411053917f16de8bb756b1d16099af9e7a7c9
Eric Roos <ericroos13@gmail.com>
2015-01-30 09:46:45 -0600

    updated key for prod

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,8 @@ class WelcomeController < ApplicationController
       @endpoint = "http://bookshout.dev/authenticate_portal.json"
       @key = "RhJHoBYtBkxLWUKsmOUQldYhMKRAfEdTpogWALmrKRBmPItcCR"
     elsif Rails.env.production?
+      @endpoint = "http://staging.bookshout.com/authenticate_portal.json"
+      @key = "yQmBzaTZIecmdrfWLZJswBNZHbalFzqPTxcqGLeJuGdZChHwtV"
     end
   end
 end
