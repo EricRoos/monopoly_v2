b5217bdddd9352b83351af7d44735d897c3d679d
Eric Roos <ericroos13@gmail.com>
2015-02-02 16:05:13 -0600

    changed qa endpoint

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,13 @@
 class WelcomeController < ApplicationController
   def index
     if Rails.env.development?
-      @endpoint = "http://bookshout.dev/authenticate_portal.json"
-      @key = "RhJHoBYtBkxLWUKsmOUQldYhMKRAfEdTpogWALmrKRBmPItcCR"
+      @development_endpoint = "http://bookshout.dev/authenticate_portal.json"
+      @development_key = "RhJHoBYtBkxLWUKsmOUQldYhMKRAfEdTpogWALmrKRBmPItcCR"
     elsif Rails.env.production?
       @staging_endpoint = "https://staging.bookshout.com/authenticate_portal.json"
       @staging_key = "yQmBzaTZIecmdrfWLZJswBNZHbalFzqPTxcqGLeJuGdZChHwtV"
 
-      @qa_endpoint= "https://qa.bookshout.com/authenticate_portal.json"
+      @qa_endpoint= "https://qa.bookshout.com/authenticatePortal.json"
       @qa_key = "oSoGyIjGdcTzIVYulxQowbzHzEazPPAEMxlSVIVitaWuSOfZji"
     end
   end
