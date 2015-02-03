3c1804f59bfad6ce14dadb9e8a74317aa1f20ef8
Eric Roos <ericroos13@gmail.com>
2015-02-03 10:40:14 -0600

    added prod to portal

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,6 +9,10 @@ class WelcomeController < ApplicationController
 
       @qa_endpoint= "https://qa.bookshout.com/authenticatePortal.json"
       @qa_key = "oSoGyIjGdcTzIVYulxQowbzHzEazPPAEMxlSVIVitaWuSOfZji"
+
+      @prod_endpoint = "https://bookshout.com/authenticate_portal.json"
+      @prod_key = "4Tk4nRcJkZ3emawrKCD27Z7X4ZV29EOaKRd9UOaxnLpLRnWI8G"
+
     end
   end
   def redirect
