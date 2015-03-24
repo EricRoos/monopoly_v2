d10d55fee81e2cc59671c76a13bf4c2053016ba4
Eric Roos <ericroos13@gmail.com>
2015-03-24 09:47:50 -0500

    got bootstrap material design up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require jquery-cookie
 //= require turbolinks
-//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,12 @@ Bundler.require(*Rails.groups)
 
 module BootstrapApp
   class Application < Rails::Application
-root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
-  config.sass.load_paths << bower_path
-  config.assets.paths << bower_path
-end
-# Precompile Bootstrap fonts
-config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
-# Minimum Sass number precision required by bootstrap-sass
-::Sass::Script::Number.precision = [8, ::Sass::Script::Number.precision].max
+    config.assets.enabled = true
+    config.less.paths << "#{Rails.root}/lib/less/protractor/stylesheets"
+    config.less.compress = true
+    root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+      config.assets.paths << bower_path
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
